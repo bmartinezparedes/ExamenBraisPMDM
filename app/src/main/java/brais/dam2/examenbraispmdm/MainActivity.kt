d4272b974fc4af3c42fba2eee3d2667412babda4
Braiskiskos <bmartinezparedes@danielcastelao.com>
2021-12-13 09:40:58 +0100

    Initial commit

---
+++ app/src/main/java/brais/dam2/examenbraispmdm/MainActivity.kt
@@ -0,0 +1,11 @@
+package brais.dam2.examenbraispmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
