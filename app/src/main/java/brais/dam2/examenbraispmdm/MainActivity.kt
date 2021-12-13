9e51b1e2697089b25c6b397cf3238095157ebf59
Braiskiskos <bmartinezparedes@danielcastelao.com>
2021-12-13 10:48:40 +0100

    comentarios codigo log + toast

--- app/src/main/java/brais/dam2/examenbraispmdm/MainActivity.kt
+++ app/src/main/java/brais/dam2/examenbraispmdm/MainActivity.kt
@@ -10,10 +10,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        /*
+        Hago un for qu e a la variable numero le vay ametiendo los valores de 100
+        a 0 y despues con el Log.i imprimo esos valores que se mostraran luego en logcat
+        en el apartado info. le puse un tag para que sea mas facil buscarlo despues
+         */
         for (numero in 100 downTo 0){
             Log.i("cuenta regresiba", numero.toString())
         }
+        /*
+        Declaro el variable boton que le meto un boton que hace referencia a una id
+        y dentro esta a la espera de ser pulsado para imprimir una Toas
+        que es un mensaje qvisible para el ususario
+         */
         val bToast: Button =findViewById(R.id.bToast)
         bToast.setOnClickListener{
             Toast.makeText(this.applicationContext, "Apruebame", Toast.LENGTH_SHORT).show()
