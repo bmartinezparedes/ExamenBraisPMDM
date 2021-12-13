df9bb4b620d47b0c4dc661ba177a99ce139dbe64
Braiskiskos <bmartinezparedes@danielcastelao.com>
2021-12-13 10:04:29 +0100

    Log + Toast

--- app/src/main/java/brais/dam2/examenbraispmdm/MainActivity.kt
+++ app/src/main/java/brais/dam2/examenbraispmdm/MainActivity.kt
@@ -2,11 +2,22 @@ package brais.dam2.examenbraispmdm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        for (numero in 100..0){
+            Log.i("cuenta regresiba", numero.toString())
+        }
+        val bToast: Button =findViewById(R.id.bToast)
+        bToast.setOnClickListener{
+            Toast.makeText(this.applicationContext, "Apruebame", Toast.LENGTH_SHORT).show()
+        }
     }
 
     override fun onResume() {
